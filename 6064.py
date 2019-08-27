7f5b5040f1d485dff6e4b9c6f8640158ae72bcee
jamie <rmk1075@gmail.com>
2019-08-27 17:15:02 +0900

    init

---
+++ 6064.py
@@ -0,0 +1,14 @@
+T = int(input())
+for t in range(T):
+    M, N, x, y = map(int, input().split())
+    count = 0
+
+    t = x
+    temp = []
+    count = 0
+    while t not in temp:
+        t = (t + M) % N
+        count +=
+
+    if y in temp:
+        print
