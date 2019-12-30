abe7568c9db6bb9d9089d1f5e49019bb71c7fd08
jamie <rmk1075@gmail.com>
2019-12-31 01:19:59 +0900

    BOJ_backtracking

--- 14889.py
+++ 14889.py
@@ -2,29 +2,59 @@ import sys
 
 N = int(sys.stdin.readline())
 S = [[i for i in list(map(int, sys.stdin.readline().strip().split()))] for _ in range(N)]
+members = [0 for _ in range(N)]
 diff = 4000
 
-def team(team1, x):
-    if len(team1) == N/2:
+def team(members, start, count):
+    if count == N/2:
         val1, val2 = 0, 0
         for i in range(N):
             for j in range(N):
                 if i != j and i < j:
-                    if i in team1 and j in team1:
+                    if members[i] == 0 and members[j] == 0:
                         val1 += S[i][j]+S[j][i]
-                    elif i not in team1 and j not in team1:
+                    elif members[i] == 1 and members[j] == 1:
                         val2 += S[i][j]+S[j][i]
         global diff
         if abs(val1-val2) < diff:
             diff = abs(val1-val2)
     else:
-        for i in range(x+1, N):
-            team1.append(i)
-            team(team1, x+1)
-            team1.remove(i)
+        for i in range(start+1, N):
+            if members[i] != 1:
+                members[i] = 1
+                team(members, i, count+1)
+                members[i] = 0
 
-team([0], 0)
+team(members, 0, 0)
 print(diff)
+
+# import sys
+
+# N = int(sys.stdin.readline())
+# S = [[i for i in list(map(int, sys.stdin.readline().strip().split()))] for _ in range(N)]
+# diff = 4000
+
+# def team(team1, x):
+#     if len(team1) == N/2:
+#         val1, val2 = 0, 0
+#         for i in range(N):
+#             for j in range(N):
+#                 if i != j and i < j:
+#                     if i in team1 and j in team1:
+#                         val1 += S[i][j]+S[j][i]
+#                     elif i not in team1 and j not in team1:
+#                         val2 += S[i][j]+S[j][i]
+#         global diff
+#         if abs(val1-val2) < diff:
+#             diff = abs(val1-val2)
+#     else:
+#         for i in range(x+1, N):
+#             team1.append(i)
+#             team(team1, x+1)
+#             team1.remove(i)
+
+# team([0], 0)
+# print(diff)
 
 # import sys
 
