a5fe9d1f009a82c3fef0249c34a0e0bd69275814
jamie <rmk1075@gmail.com>
2020-02-05 22:22:13 +0900

    BOJ_Samsung_SW_test_15683_java

--- 14891/Main.java
+++ 14891/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
