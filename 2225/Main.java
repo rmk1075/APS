dfae6975e07b1fb90a6f1db884b44d221179f574
jamie <rmk1075@gmail.com>
2020-02-20 01:26:32 +0900

    BOJ_dp_2225_java

---
+++ 2225/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int N, K;
+    static int[][] nums;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        sc.close();
+        nums = new int[K+1][N+1];
+
+        nums[0][0] = 1;
+        for(int i = 0; i < K; i++) {
+            for(int j = 0; j < N+1; j++) {
+                for(int k = 0; k < N+1; k++) {
+                    if(nums[i][k] != 0 && j+k < N+1) {
+                        nums[i+1][j+k] = (nums[i+1][j+k] + nums[i][k]) % 1_000_000_000;
+                    }
+                }
+            }
+        }
+
+        // TODO:
+        // for(int i = 0; i < K+1; i++) {
+        //     System.out.println(Arrays.toString(nums[i]));
+        // }
+
+        System.out.println(nums[K][N]);
+    }
+}
