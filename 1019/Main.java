d6b9b4e6551dc146dd7d41803e91fa569ae5a809
jamie <rmk1075@gmail.com>
2020-03-06 09:49:06 +0900

    1019_sb

--- 1019/Main.java
+++ 1019/Main.java
@@ -5,25 +5,28 @@ import java.io.InputStreamReader;
 public class Main {
     static int N, src = 1, offset = 1;
     static int[] arr = new int[10];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
 
-        while(src <= N) {
-            while(N % 10 != 9 && src <= N) {
+        while (src <= N) {
+            while (N % 10 != 9 && src <= N) {
                 int x = N;
-                while(0 < x) {
+                while (0 < x) {
                     arr[x % 10] += offset;
                     x /= 10;
                 }
                 N--;
             }
 
-            if(N < src) break;
+            if (N < src)
+                break;
 
-            while(src % 10 != 0 && src <= N) {
+            while (src % 10 != 0 && src <= N) {
                 int x = src;
-                while(0 < x) {
+                while (0 < x) {
                     arr[x % 10] += offset;
                     x /= 10;
                 }
@@ -33,13 +36,58 @@ public class Main {
             src /= 10;
             N /= 10;
 
-            for(int i = 0; i < 10; i++) arr[i] += (N - src + 1) * offset;
+            for (int i = 0; i < 10; i++)
+                arr[i] += (N - src + 1) * offset;
             offset *= 10;
         }
-        
-        for(int n : arr) System.out.print(n + " ");
+
+        for (int n : arr) sb.append(n + " ");
+        System.out.println(sb.toString());
     }
 }
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+//     static int N, src = 1, offset = 1;
+//     static int[] arr = new int[10];
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         N = Integer.parseInt(br.readLine());
+
+//         while(src <= N) {
+//             while(N % 10 != 9 && src <= N) {
+//                 int x = N;
+//                 while(0 < x) {
+//                     arr[x % 10] += offset;
+//                     x /= 10;
+//                 }
+//                 N--;
+//             }
+
+//             if(N < src) break;
+
+//             while(src % 10 != 0 && src <= N) {
+//                 int x = src;
+//                 while(0 < x) {
+//                     arr[x % 10] += offset;
+//                     x /= 10;
+//                 }
+//                 src++;
+//             }
+
+//             src /= 10;
+//             N /= 10;
+
+//             for(int i = 0; i < 10; i++) arr[i] += (N - src + 1) * offset;
+//             offset *= 10;
+//         }
+        
+//         for(int n : arr) System.out.print(n + " ");
+//     }
+// }
 
 // import java.util.Scanner;
 
