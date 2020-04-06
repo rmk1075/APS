ed4eb47d9b996a7a126b80c2da597d89ef47b842
jamie <rmk1075@gmail.com>
2020-04-06 12:39:03 +0900

    11657_bellman_ford_update_java

--- 11657/Main.java
+++ 11657/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -16,43 +15,46 @@ class Edge {
 }
 
 public class Main {
-    static int N, M, nodes[];
+    static int N, M, nodes[], edges[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         nodes = new int[N+1];
-        Arrays.fill(nodes, Integer.MAX_VALUE);
+        edges = new int[N+1][N+1];
+        for(int i = 0; i <= N; i++) {
+            nodes[i] = Integer.MAX_VALUE;
+            Arrays.fill(edges[i], Integer.MAX_VALUE);
+        }
         
-        ArrayList<Edge> edges = new ArrayList<>();
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            edges.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            int u = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken()), w = Integer.parseInt(st.nextToken());
+            edges[u][v] = Math.min(edges[u][v], w);
         }
 
         nodes[1] = 0;
         int count = 0;
         boolean isUpdated = true;
-        while(isUpdated && count <= N) {
+        while(count++ < N && isUpdated) {
             isUpdated = false;
-            
-            for(Edge edge : edges) {
-                if(nodes[edge.src] == Integer.MAX_VALUE || nodes[edge.dest] <= nodes[edge.src] + edge.weight) continue;
-                
-                nodes[edge.dest] = nodes[edge.src] + edge.weight;
-                isUpdated = true;
+            for(int i = 1; i <= N; i++) {
+                for(int j = 1; j <= N; j++) {
+                    if(edges[i][j] == Integer.MAX_VALUE || nodes[i] == Integer.MAX_VALUE || nodes[j] <= nodes[i] + edges[i][j]) continue;
+                    nodes[j] = nodes[i] + edges[i][j];
+                    isUpdated = true;
+                }
             }
-            count++;
         }
 
-        if(N < count) {
+        if(isUpdated) {
             System.out.println(-1);
             System.exit(0);
         }
-
+        
         StringBuilder sb = new StringBuilder();
-        for(int i = 2; i <= N; i++) sb.append(((nodes[i] == Integer.MAX_VALUE) ? -1 : nodes[i])).append("\n");
+        for(int i = 2; i <= N; i++) sb.append(((nodes[i] == Integer.MAX_VALUE) ? "-1\n" : nodes[i]+"\n"));
         System.out.println(sb);
     }
 }
