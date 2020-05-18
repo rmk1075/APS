7bc128bc8ae06f568f3c832c0c349330da15acb1
jamie <rmk1075@gmail.com>
2020-05-18 23:44:02 +0900

    BOJ_1239_arraylist_java

--- 1239/Main.java
+++ 1239/Main.java
@@ -1,3 +1,59 @@
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.ArrayList;
+// import java.util.StringTokenizer;
+
+// public class Main {
+//     static int N, visited = 0, maxCnt = 0, arr[], order[];
+//     static ArrayList<Integer> vals = new ArrayList<>();
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         N = Integer.parseInt(br.readLine());
+//         arr = new int[N];
+//         order = new int[N];
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+//         for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+//         for(int i = 0; i < N; i++) {
+//             visited |= 1 << i;
+//             order[0] = i;
+//             check(1);
+//             visited &= ~(1 << i);
+//         }
+//         System.out.println(maxCnt);
+//     }
+
+//     public static void check(int count) {
+//         if(count == N) {
+//             vals.clear();
+//             vals.add(0);
+//             int temp = 0;
+//             for(int i = 0; i < N-1; i++) {
+//                 temp += arr[order[i]];
+//                 vals.add(temp);
+//             }
+
+//             int cnt = 0;
+//             for(int i : vals) {
+//                 if(49 < i) break;
+//                 if(vals.contains(i + 50)) cnt++;
+//             }
+
+//             maxCnt = Math.max(maxCnt, cnt);
+//             return ;
+//         }
+
+//         for(int i = 0; i < N; i++) {
+//             if((visited & (1 << i)) == 0) {
+//                 visited |= 1 << i;
+//                 order[count] = i;
+//                 check(count + 1);
+//                 visited &= ~(1 << i);
+//             }
+//         }
+//     }
+// }
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
