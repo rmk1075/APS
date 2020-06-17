a422d08e540a5df4b49123a0f64c4fd96db35a6c
jamie <rmk1075@gmail.com>
2020-06-18 00:20:41 +0900

    BOJ_19236_dfs_java

--- 19236/Main.java
+++ 19236/Main.java
@@ -4,120 +4,125 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int sum, fishes[][] = new int[17][2], map[][][] = new int[4][4][2]; // 0: index, 1: direction
+    static int sum;
     static int[] dx = {0, -1, -1, 0, 1, 1, 1, 0, -1}, dy = {0, 0, -1, -1, -1, 0, 1, 1, 1};
     public static void main(String[] args) throws IOException {
-        doInit();
-        dfs(sum, fishes, map);
+        int fishes[][] = new int[17][3], map[][] = new int[4][4]; // fishes: 0 - x, 1 - y, 2 - dir | map: idx
+        doInit(fishes, map);
+        dfs(fishes, map, sum);
         System.out.println(sum);
     }
 
-    public static void dfs(int sum_, int[][] fishes_, int[][][] map_) {
-        int x, y, d;
-        int[][] fishes__ = new int[17][2];
-        for(int i = 0; i < 17; i++) {
-            fishes__[i][0] = fishes_[i][0];
-            fishes__[i][1] = fishes_[i][1];
-        }
+    public static void dfs(int[][] fishes_, int[][] map_, int val) {
+        int fishes[][] = new int[17][3], map[][] = new int[4][4];
         
-        int[][][] map__ = new int[4][4][2];
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                map__[i][j][0] = map_[i][j][0];
-                map__[i][j][1] = map_[i][j][1];
-            }
-        }
+        // copy arrays
+        copy(fishes_, map_, fishes, map);
+
+        // fish move
+        fish(fishes, map);
         
-        // fishes
-        for(int i = 1; i < 17; i++) {
-            int fx = fishes__[i][0], fy = fishes__[i][1];
-            if(map__[fx][fy][0] != i) continue;
+        // shark move
+        shark(fishes, map, val);
 
-            d = map__[fx][fy][1];
-            do {
-                x = fx + dx[d];
-                y = fy + dy[d];
-                if(x < 0 || y < 0 || x == 4 || y == 4 || map__[x][y][0] == 0) {
-                    d--;
-                    if(d < 1) d = 8;
-                    continue;
-                }
+        sum = Math.max(sum, val);
+    }
 
-                if(map__[x][y][0] == -1) {
-                    map__[fx][fy][0] = -1;
-                    map__[x][y][0] = i;
-                    map__[x][y][1] = d;
-                    fishes__[i][0] = x;
-                    fishes__[i][1] = y;
-                    break;
-                }
-                
-                // change
-                int tempIdx = map__[x][y][0], tempDir = map__[x][y][1], tempX = x, tempY = y;
-
-                fishes__[tempIdx][0] = fx;
-                fishes__[tempIdx][1] = fy;
-                
-                fishes__[i][0] = tempX;
-                fishes__[i][1] = tempY;
-
-                map__[x][y][0] = i;
-                map__[x][y][1] = d;
-
-                map__[fx][fy][0] = tempIdx;
-                map__[fx][fy][1] = tempDir;
-                
-                break;
-            } while(d != map__[fx][fy][1]);
-        }
-
-        // shark
-        int sharkX = fishes__[0][0], sharkY = fishes__[0][1];
-        map__[sharkX][sharkY][0] = -1;
-        d = map__[sharkX][sharkY][1];
-        x = sharkX;
-        y = sharkY;
+    public static void shark(int[][] fishes, int[][] map, int val) {
+        int x = fishes[0][0], y = fishes[0][1], d = fishes[0][2];
+        map[x][y] = -1;
         while(true) {
             x += dx[d];
             y += dy[d];
-            if(x < 0 || y < 0 || x == 4 || y == 4) break;
-            if(map__[x][y][0] == -1) continue;
 
-            // eat
-            int fish = map__[x][y][0];
-            map__[x][y][0] = 0;
-            fishes__[0][0] = x;
-            fishes__[0][1] = y;
+            if(x < 0 || y < 0 || 3 < x || 3 < y) break;
+            if(map[x][y] == -1) continue;
 
-            dfs(sum_ + fish, fishes__, map__);
-            
-            map__[x][y][0] = fish;
+            int feed = map[x][y];
+            map[x][y] = 0;
+            fishes[0][0] = x;
+            fishes[0][1] = y;
+            fishes[0][2] = fishes[feed][2];
+
+            dfs(fishes, map, val + feed);
+
+            map[x][y] = feed;
         }
-
-        sum = Math.max(sum, sum_);
     }
 
-    public static void doInit() throws IOException {
-        input();
+    public static void fish(int[][] fishes, int[][] map) {
+        int x, y, d;
+        for(int i = 1; i < 17; i++) {
+            x = fishes[i][0];
+            y = fishes[i][1];
+            if(map[x][y] != i) continue;
 
-        sum = map[0][0][0];
-        map[0][0][0] = 0;
-    }
+            while(true) {
+                int x_ = x + dx[fishes[i][2]], y_ = y + dy[fishes[i][2]];
+                if(x_ < 0 || y_ < 0 || x_ == 4 || y_ == 4 || map[x_][y_] == 0) {
+                    fishes[i][2]++;
+                    if(8 < fishes[i][2]) fishes[i][2] = 1;
+                } else {
+                    // change
+                    if(map[x_][y_] == -1) {
+                        // empty
+                        map[x][y] = -1;
+                        map[x_][y_] = i;
+    
+                        fishes[i][0] = x_;
+                        fishes[i][1] = y_;
+                    } else {
+                        // non-empty
+                        int target = map[x_][y_], a = fishes[i][0], b = fishes[i][1], c = fishes[i][2];
+                        fishes[i][0] = x_;
+                        fishes[i][1] = y_;
+                        // fishes[i][2] = fishes[target][2];
+                        map[x_][y_] = i;
 
-    public static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        for (int i = 0; i < 4; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-
-                map[i][j][0] = a; // fish no
-                map[i][j][1] = b; // fish dir
-
-                fishes[a][0] = i; // a-th fish loc i
-                fishes[a][1] = j; // a-th fish loc j
+                        fishes[target][0] = a;
+                        fishes[target][1] = b;
+                        // fishes[target][2] = c;
+                        map[a][b] = target;
+                    }
+                    break;
+                }
             }
         }
+    }
+
+    public static void copy(int[][] A_, int[][] B_, int [][] A, int[][] B) {
+        for(int i = 0; i < 17; i++) {
+            A[i][0] = A_[i][0];
+            A[i][1] = A_[i][1];
+            A[i][2] = A_[i][2];
+        }
+
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) B[i][j] = B_[i][j];
+        }
+    }
+
+    public static void doInit(int[][] fishes, int[][] map) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // read input
+        for(int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 4; j++) {
+                int idx = Integer.parseInt(st.nextToken()), dir = Integer.parseInt(st.nextToken());
+
+                fishes[idx][0] = i;
+                fishes[idx][1] = j;
+                fishes[idx][2] = dir;
+
+                map[i][j] = idx;
+            }
+        }
+
+        // shark init
+        sum = map[0][0];
+        fishes[0][2] = fishes[sum][2];
+        map[0][0] = 0;
     }
 }
