65b735b1fb37f7aeafc4145fd7fa5dcf98dfc2d7
jamie <rmk1075@gmail.com>
2020-08-22 16:03:57 +0900

    programmers_[1차]_프렌즈4블록_java

---
+++ programmers/[1차]_프렌즈4블록/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution(4, 5, new String[]{"CCBDE", "AAADE", "AAABF", "CCBBF"});
+    }    
+}
