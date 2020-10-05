baa50a603aec890b5dbb4d20697b6db3d245178e
jamie <rmk1075@gmail.com>
2020-10-05 23:01:55 +0900

    programmers_징검다리_건너기_java_binarySearch

---
+++ programmers/징검다리_건너기/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        System.out.println(solution.solution(new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1}, 3));
+    }
+}
