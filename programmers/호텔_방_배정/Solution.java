a6b035f7b0a6ff55f1f720f7ad42311f9181a681
jamie <rmk1075@gmail.com>
2020-12-04 02:20:00 +0900

    programmers_호텔_방_배정_update_to_recursion_java

--- programmers/호텔_방_배정/Solution.java
+++ programmers/호텔_방_배정/Solution.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class Solution {
@@ -10,35 +8,66 @@ class Solution {
         Map<Long, Long> map = new HashMap<>();
         int index = 0;
         for (long number : room_number) {
-            if (!map.containsKey(number)) {
-                answer[index++] = number;
-                map.put(number, find(map, number + 1));
-                continue;
-            }
-
-            long temp = find(map, number + 1);
-            answer[index++] = temp;
-            map.put(temp, find(map, temp + 1));
+            answer[index++] = find(map, number);
         }
 
         return answer;
     }
 
     public long find(Map<Long, Long> map, long number) {
-        List<Long> list = new ArrayList<>();
-        long result = number;
-        while (true) {
-            if (map.containsKey(result)) {
-                list.add(result);
-                result = map.get(result);
-            } else
-                break;
+        if (!map.containsKey(number)) {
+            map.put(number, number + 1);
+            return number;
         }
 
-        for (long num : list) {
-            map.put(num, result);
-        }
+        long result = find(map, map.get(number));
+        map.put(number, result);
 
         return result;
     }
 }
+
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+// class Solution {
+// public long[] solution(long k, long[] room_number) {
+// long[] answer = new long[room_number.length];
+
+// Map<Long, Long> map = new HashMap<>();
+// int index = 0;
+// for (long number : room_number) {
+// if (!map.containsKey(number)) {
+// answer[index++] = number;
+// map.put(number, find(map, number + 1));
+// continue;
+// }
+
+// long temp = find(map, number + 1);
+// answer[index++] = temp;
+// map.put(temp, find(map, temp + 1));
+// }
+
+// return answer;
+// }
+
+// public long find(Map<Long, Long> map, long number) {
+// List<Long> list = new ArrayList<>();
+// long result = number;
+// while (true) {
+// if (map.containsKey(result)) {
+// list.add(result);
+// result = map.get(result);
+// } else
+// break;
+// }
+
+// for (long num : list) {
+// map.put(num, result);
+// }
+
+// return result;
+// }
+// }
