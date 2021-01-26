ff9038832d092f84d30d99b6579e2082dc87d56c
jamie <rmk1075@gmail.com>
2021-01-26 22:48:59 +0900

    boj_1931_greedy_java

--- boj/1931/Main.java
+++ boj/1931/Main.java
@@ -1,36 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
 
-        int N = sc.nextInt();
-        int[][] meetings = new int[N][2];
-        for(int i = 0; i < N; i++) {
-            meetings[i][0] = sc.nextInt();
-            meetings[i][1] = sc.nextInt();
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        sc.close();
 
-        Arrays.sort(meetings, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(a[0], b[0]);
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                return o1[1] - o2[1];
             }
         });
 
-        int count = 1;
-        int end = meetings[0][1];
-        for(int i = 1; i < N; i++) {
-            if(end <= meetings[i][0]) {
-                count++;
-                end = meetings[i][1];
-            } else if(meetings[i][1] <= end) {
-                end = meetings[i][1];
-            }
+        int cnt = 0, temp = -1;
+        for (int[] a : arr) {
+            if (a[0] < temp)
+                continue;
+
+            cnt++;
+            temp = a[1];
         }
 
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
