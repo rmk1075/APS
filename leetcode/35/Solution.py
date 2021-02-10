821ca7fa6377d9b213f8213adc225eaa8b2caa3f
jamie <rmk1075@gmail.com>
2021-02-10 13:13:54 +0900

    leetcode_35_modifying

--- leetcode/35/Solution.py
+++ leetcode/35/Solution.py
@@ -1,17 +1,31 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right= 0, len(nums) - 1
-        mid = int((left + right) / 2)
-        while left < right:
-            mid = int((left + right) / 2)
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 left = mid + 1
             else:
-                right = mid
+                right = mid - 1
         
-        if nums[left] < target:
-            return left + 1
-        else:
-            return left
+        return left
+
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         left, right= 0, len(nums) - 1
+#         mid = int((left + right) / 2)
+#         while left < right:
+#             mid = int((left + right) / 2)
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid
+        
+#         if nums[left] < target:
+#             return left + 1
+#         else:
+#             return left
