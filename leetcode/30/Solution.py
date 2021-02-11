b8bf1b5aa6d500fd45aebfa7cacc0cd7c580e25a
jamie <rmk1075@gmail.com>
2021-02-12 01:04:35 +0900

    leetcode_30_Substring with Concatenation of All Words _python

--- leetcode/30/Solution.py
+++ leetcode/30/Solution.py
@@ -1,35 +1,34 @@
+from collections import Counter, defaultdict
+
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        slen, wlen = len(s), len(words)
-        indexList = list()
-        for i in range(slen):
-            tempList = list()
-            for j in range(wlen):
-                if i + len(words[j]) <= slen and s[i:i + len(words[j])] == words[j]:
-                    tempList.append(j)
-            indexList.append(tempList)
+        answer, wordsMap = list(), Counter(words)
+        totalLen, wlen = len(words) * len(words[0]), len(words[0])
 
-        answer = list()
-        for i in range(slen):
-            if self.find(0, i, slen, wlen, indexList, words, [False] * wlen):
-                answer.append(i)
+        # 시작점 경우의 수 - wlen
+        for i in range(wlen):
+            start, count = i, 0
+            countMap = defaultdict(int)
 
+            # 각 시작점에서 s에 대한 반복문
+            for j in range(i, len(s) - wlen + 1, wlen):
+                word = s[j:j + wlen]
+
+                if word in wordsMap:
+                    countMap[word] += 1
+                    count += 1
+
+                    # 중복되는 word의 발생으로 countMap의 값이 wordsMap의 값보다 큰 경우
+                    # 해당 word 제거를 위해서 start window 이돔 및 countMap에서 제거
+                    while wordsMap[word] < countMap[word]:
+                        countMap[s[start:start + wlen]] -= 1
+                        count -= 1
+                        start += wlen
+                    
+                    if count == len(words):
+                        answer.append(start)
+                else:
+                    start, count = j + wlen, 0
+                    countMap = defaultdict(int)
+                
         return answer
-
-    def find(self, count: int, index: int, slen: int, wlen: int, indexList: List[List[int]], words: List[str], visited: List[bool]) -> bool:
-        if count == wlen:
-            return True
-
-        if slen <= index:
-            return False
-
-        for i in indexList[index]:
-            l = index + len(words[i])
-
-            if not visited[i]:
-                visited[i] = True
-                if self.find(count + 1, l, slen, wlen, indexList, words, visited):
-                    return True
-                visited[i] = False
-        
-        return False
