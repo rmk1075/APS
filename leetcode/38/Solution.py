30ee88c1bdcbdfd57e70340e0d74a9b26ad2ff09
jamie <rmk1075@gmail.com>
2021-02-15 02:19:17 +0900

    leetcode_38_upgrage

--- leetcode/38/Solution.py
+++ leetcode/38/Solution.py
@@ -2,10 +2,8 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
-        return self.count(self.countAndSay(n - 1))
-        
-    def count(self, seq: str) -> str:
-        result = str()
+
+        result, seq = str(), self.countAndSay(n - 1)
         num, cnt = seq[0], 0
         for s in seq:
             if s == num:
@@ -14,6 +12,26 @@ class Solution:
                 result += str(cnt) + num
                 num = s
                 cnt = 1
-        
         result += str(cnt) + num
+
         return result
+
+# class Solution:
+#     def countAndSay(self, n: int) -> str:
+#         if n == 1:
+#             return "1"
+#         return self.count(self.countAndSay(n - 1))
+        
+#     def count(self, seq: str) -> str:
+#         result = str()
+#         num, cnt = seq[0], 0
+#         for s in seq:
+#             if s == num:
+#                 cnt += 1
+#             else:
+#                 result += str(cnt) + num
+#                 num = s
+#                 cnt = 1
+        
+#         result += str(cnt) + num
+#         return result
