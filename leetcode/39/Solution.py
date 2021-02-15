c50f85742ac0af8984851936b154268767c73d48
jamie <rmk1075@gmail.com>
2021-02-15 16:30:11 +0900

    leetcode_39_Combination Sum_dfs_python

---
+++ leetcode/39/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        result = []
+        self.dfs(result, candidates, target, 0, 0, [])
+
+        return result
+    
+    def dfs(self, result: List[List[int]],candidates: List[int], target: int, index: int, value: int, tempList: List[int]):
+        temp = tempList[:]
+
+        for i in range(index, len(candidates)):
+            num = candidates[i]
+            if target == value + num:
+                temp.append(num)
+                result.append(temp)
+                return
+            
+            if target < value + num:
+                continue
+            
+            temp.append(num)
+            self.dfs(result, candidates, target, i, value + num, temp)
+            temp.pop()
