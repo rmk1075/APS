e67c1e1699fb0f14aea07cc7f33c448312bf6957
jamie <rmk1075@gmail.com>
2021-02-21 20:56:12 +0900

    leetcode_45_update

--- leetcode/45/Solution.py
+++ leetcode/45/Solution.py
@@ -1,3 +1,4 @@
+# 168ms / 16.1MB
 class Solution:
     def jump(self, nums: List[int]) -> int:
         if len(nums) == 1:
@@ -13,11 +14,63 @@ class Solution:
                 if end < distance:
                     return cnt
                 
+                if distance <= right:
+                    continue
+                
                 for i in range(right, distance):
                     queue.append(i)
                 
-                right = max(right, distance)
+                right = distance
             
             cnt += 1
 
         return cnt
+
+# 176ms / 17.7MB
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         if len(nums) == 1:
+#             return 0
+
+#         right, end = 1, len(nums) - 1
+#         queue = [(0, 1)]
+#         while queue:
+#             current, cnt = queue.pop(0)
+#             distance = current + nums[current] + 1
+#             if end < distance:
+#                 return cnt
+            
+#             if distance <= right:
+#                 continue
+            
+#             for i in range(right, distance):
+#                 queue.append((i, cnt + 1))
+            
+#             right = distance
+
+#         return cnt
+
+# 176ms / 16.3MB
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         if len(nums) == 1:
+#             return 0
+
+#         left, right, end = 0, 1, len(nums) - 1
+#         cnt, queue = 1, [0]
+#         while queue:
+#             size = len(queue)
+#             for _ in range(size):
+#                 current = queue.pop(0)
+#                 distance = current + nums[current] + 1
+#                 if end < distance:
+#                     return cnt
+                
+#                 for i in range(right, distance):
+#                     queue.append(i)
+                
+#                 right = max(right, distance)
+            
+#             cnt += 1
+
+#         return cnt
