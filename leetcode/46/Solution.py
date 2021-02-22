c8186cabbcbdb53acc85d9ffa964cafa14957423
jamie <rmk1075@gmail.com>
2021-02-22 14:03:39 +0900

    leetcode_46_update

--- leetcode/46/Solution.py
+++ leetcode/46/Solution.py
@@ -3,19 +3,39 @@ class Solution:
         length = len(nums)
         result = []
         for i in range(len(nums)):
-            result += self.dfs(nums, length, 1, i, 1 << i, [nums[i]])
+            self.dfs(nums, length, 1, i, 1 << i, [nums[i]], result)
         
         return result
     
-    def dfs(self, nums: List[int], length: int, cnt : int, index: int, visited: int, tempList: List[int]) -> List[List[int]]:
+    def dfs(self, nums: List[int], length: int, cnt : int, index: int, visited: int, tempList: List[int], result: List[List[int]]):
         if cnt == length:
-            return [tempList]
+            result.append(tempList)
+            return
         
-        result = []
         for i in range(length):
             if (visited & (1 << i)) != 0:
                 continue
             
-            result += self.dfs(nums, length, cnt + 1, i, visited | (1 << i), tempList + [nums[i]])
+            self.dfs(nums, length, cnt + 1, i, visited | (1 << i), tempList + [nums[i]], result)
 
-        return result
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         length = len(nums)
+#         result = []
+#         for i in range(len(nums)):
+#             result += self.dfs(nums, length, 1, i, 1 << i, [nums[i]])
+        
+#         return result
+    
+#     def dfs(self, nums: List[int], length: int, cnt : int, index: int, visited: int, tempList: List[int]) -> List[List[int]]:
+#         if cnt == length:
+#             return [tempList]
+        
+#         result = []
+#         for i in range(length):
+#             if (visited & (1 << i)) != 0:
+#                 continue
+            
+#             result += self.dfs(nums, length, cnt + 1, i, visited | (1 << i), tempList + [nums[i]])
+
+#         return result
