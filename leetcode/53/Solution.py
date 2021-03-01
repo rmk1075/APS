2708d6afc940121ad0c8421657b2e0306b40f316
jamie <rmk1075@gmail.com>
2021-03-01 11:48:52 +0900

    leetcode_53_update

--- leetcode/53/Solution.py
+++ leetcode/53/Solution.py
@@ -1,9 +1,23 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = nums[:]
-        for i in range(1, n):
-            if dp[i] < dp[i - 1] + nums[i]:
-                dp[i] = dp[i - 1] + nums[i]
+        maxVal, val = nums[0], nums[0]
+        for num in nums[1:]:
+            if num < val + num:
+                val += num
+            else:
+                val = num
+                
+            if maxVal < val:
+                maxVal = val
+                
+        return maxVal
 
-        return max(dp)
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         dp = nums[:]
+#         for i in range(1, n):
+#             if dp[i] < dp[i - 1] + nums[i]:
+#                 dp[i] = dp[i - 1] + nums[i]
+
+#         return max(dp)
