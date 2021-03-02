4dbb2d654b8369d94fd474089ae868e61f9050be
jamie <rmk1075@gmail.com>
2021-03-02 13:00:13 +0900

    leetcode_55_update_end to src

--- leetcode/55/Solution.py
+++ leetcode/55/Solution.py
@@ -1,14 +1,24 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n, left, right = len(nums) - 1, 0, nums[0]
-        while left < right:
-            distance = right
-            for index in range(left, right + 1):
-                distance = max(distance, index + nums[index])
-                if n <= distance:
-                    return True
+        dest = len(nums) - 1
+        current = dest - 1
+        while -1 < current:
+            if dest - current <= nums[current]:
+                dest = current
+            current -= 1   
+        return dest == 0
 
-            left = right
-            right = distance
+#  class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n, left, right = len(nums) - 1, 0, nums[0]
+#         while left < right:
+#             distance = right
+#             for index in range(left, right + 1):
+#                 distance = max(distance, index + nums[index])
+#                 if n <= distance:
+#                     return True
 
-        return n <= right
+#             left = right
+#             right = distance
+
+#         return n <= right
