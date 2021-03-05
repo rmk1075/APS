f62823c2aafe6704200fe3ae2f1be37abd1c067c
jamie <rmk1075@gmail.com>
2021-03-05 20:50:40 +0900

    leetcode_58_update

--- leetcode/58/Solution.py
+++ leetcode/58/Solution.py
@@ -1,14 +1,19 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        n = len(s)
+        s = s.strip().split(' ')
+        return 0 if len(s) == 0 else len(s[-1])
 
-        if n == 0:
-            return 0
-            
-        i = n - 1
-        while -1 < i:
-            if s[i] == ' ':
-                break
-            i -= 1
-        return n - i - 1
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         s = s.strip()
+#         n = len(s)
+
+#         if n == 0:
+#             return 0
+
+#         i = n - 1
+#         while -1 < i:
+#             if s[i] == ' ':
+#                 break
+#             i -= 1
+#         return n - i - 1
