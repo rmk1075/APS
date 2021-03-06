9a8ef497d6767432ee520434a584c0f0fe1dc17d
jamie <rmk1075@gmail.com>
2021-03-06 13:50:27 +0900

    leetcode_60_update_math_calculate factorial

--- leetcode/60/Solution.py
+++ leetcode/60/Solution.py
@@ -1,28 +1,45 @@
 import math
 
 class Solution:
-    count = 0
     def getPermutation(self, n: int, k: int) -> str:
-        for i in range(1, n + 1):
-            cnt = math.factorial(n - 1)
-            if k <= cnt:
-                return self.permutation(n, k, 1, 1 << i, str(i))
-            k -= cnt
+        return self.cretaePrefix(n, k, 0, '')
 
-    def permutation(self, n: int, k: int, length: int, visited: int, temp: str) -> str:
-        if length == n:
-            self.count += 1
-            return temp if self.count == k else ''
-
+    def cretaePrefix(self, n: int, k: int, cnt: int, prefix: str) -> str:
         for i in range(1, n + 1):
-            if visited & (1 << i):
-                continue
+            if str(i) not in prefix:
+                nums = math.factorial(n - cnt - 1)
+                if k <= nums:
+                    prefix += str(i)
+                    return self.cretaePrefix(n, k, cnt + 1, prefix)
+                k -= nums
+        return prefix
+
+# fix first character
+# import math
+
+# class Solution:
+#     count = 0
+#     def getPermutation(self, n: int, k: int) -> str:
+#         for i in range(1, n + 1):
+#             cnt = math.factorial(n - 1)
+#             if k <= cnt:
+#                 return self.permutation(n, k, 1, 1 << i, str(i))
+#             k -= cnt
+
+#     def permutation(self, n: int, k: int, length: int, visited: int, temp: str) -> str:
+#         if length == n:
+#             self.count += 1
+#             return temp if self.count == k else ''
+
+#         for i in range(1, n + 1):
+#             if visited & (1 << i):
+#                 continue
             
-            visited |= 1 << i
-            temp += str(i)
-            p = self.permutation(n, k, length + 1, visited, temp)
-            if p:
-                return p
+#             visited |= 1 << i
+#             temp += str(i)
+#             p = self.permutation(n, k, length + 1, visited, temp)
+#             if p:
+#                 return p
 
-            temp = temp[:-1]
-            visited &= ~(1 << i)
+#             temp = temp[:-1]
+#             visited &= ~(1 << i)
