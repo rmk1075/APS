8608cf914c2eb11996227b9cdad02e27fbb68320
jamie <rmk1075@gmail.com>
2021-03-13 16:47:13 +0900

    leetcode_69_Sqrt(x)_binarySearch_python

---
+++ leetcode/69/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x in (0, 1):
+            return x
+        
+        left, right = 0, x
+        while left < right:
+            mid = (left + right) // 2
+            if x == mid**2:
+                return mid
+            elif x < mid**2:
+                right = mid
+            else:
+                left = mid + 1
+        mid = (left + right) // 2
+        if mid ** 2 <= x:
+            return mid
+        else:
+            return mid - 1
