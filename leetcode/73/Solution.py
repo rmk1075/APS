8f6cb7f1524d3fb9e242cc439a8f78ea8146e334
jamie <rmk1075@gmail.com>
2021-03-17 13:25:31 +0900

    leetcode_73_update_set

--- leetcode/73/Solution.py
+++ leetcode/73/Solution.py
@@ -1,18 +1,32 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m, n = len(matrix), len(matrix[0])
-        row, col = [False for _ in range(m)], [False for _ in range(n)]
+        row, col = set(), set()
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    row[i] = True
-                    col[j] = True
+                    row.add(i)
+                    col.add(j)
+        for i in range(m):
+            for j in range(n):
+                if i in row or j in col:
+                    matrix[i][j] = 0
+
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         m, n = len(matrix), len(matrix[0])
+#         row, col = [False for _ in range(m)], [False for _ in range(n)]
+#         for i in range(m):
+#             for j in range(n):
+#                 if matrix[i][j] == 0:
+#                     row[i] = True
+#                     col[j] = True
         
-        for r in range(m):
-            if row[r]:
-                matrix[r] = [0 for _ in range(n)]
+#         for r in range(m):
+#             if row[r]:
+#                 matrix[r] = [0 for _ in range(n)]
         
-        for c in range(n):
-            if col[c]:
-                for r in range(m):
-                    matrix[r][c] = 0
+#         for c in range(n):
+#             if col[c]:
+#                 for r in range(m):
+#                     matrix[r][c] = 0
