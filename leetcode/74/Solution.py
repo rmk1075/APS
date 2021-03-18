133b6bbac722b6654e21681e27437100bdd3572b
jamie <rmk1075@gmail.com>
2021-03-18 16:48:22 +0900

    leetcode_74_upgrade

--- leetcode/74/Solution.py
+++ leetcode/74/Solution.py
@@ -1,14 +1,42 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
         m, n = len(matrix), len(matrix[0])
-        left, right = 0, m * n
+        i = 0
+        while i < m - 1:
+            if matrix[i][0] <= target and target < matrix[i + 1][0]:
+                break
+            i += 1
+        
+        if matrix[i][0] == target:
+            return True
+        
+        if target < matrix[i][0] or matrix[i][n - 1] < target:
+            return False
+        
+        left, right = 0, n
         while left < right:
             mid = (left + right) // 2
-            val = matrix[mid // n][mid % n]
+            val = matrix[i][mid]
             if val == target:
                 return True
             elif val < target:
                 left = mid + 1
             else:
                 right = mid
+        
         return False
+
+# class Solution(object):
+#     def searchMatrix(self, matrix, target):
+#         m, n = len(matrix), len(matrix[0])
+#         left, right = 0, m * n
+#         while left < right:
+#             mid = (left + right) // 2
+#             val = matrix[mid // n][mid % n]
+#             if val == target:
+#                 return True
+#             elif val < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid
+#         return False
