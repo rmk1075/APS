7bc03fbf63a803989a5f27463e40c3829a35a32d
jamie <rmk1075@gmail.com>
2021-03-22 21:56:51 +0900

    leetcode_78_upgrade

--- leetcode/78/Solution.py
+++ leetcode/78/Solution.py
@@ -1,25 +1,60 @@
 class Solution(object):
     def subsets(self, nums):
-        nums.sort()
-        result = [[]]
-        N, past = len(nums), -99
+        numsDict = dict()
+        for num in nums:
+            if num in numsDict:
+                numsDict[num] += 1
+            else:
+                numsDict[num] = 1
+        result, numsList = [[]], sorted(list(set(nums)))
+        N = len(numsList)
         for i in range(N):
-            if nums[i] == past:
-                continue
-            result.append([nums[i]])
-            past = nums[i]
-            self.find(N, nums, i + 1, [nums[i]], result)
+            num = numsList[i]
+            result.append([num])
+            numsDict[num] -= 1
+            self.find(N, numsDict, numsList, i, [num], result)
+            numsDict[num] += 1
 
         return result
 
-    def find(self, N, nums, idx, temp, result):
+    def find(self, N, numsDict, numsList, idx, temp, result):
+        # TODO:
+        print(numsDict)
+
         if idx == N:
             return
 
-        past = -99
         for i in range(idx, N):
-            if nums[i] == past:
+            if numsDict[numsList[i]] == 0:
                 continue
-            result.append(temp + [nums[i]])
-            past = nums[i]
-            self.find(N, nums, i + 1, temp + [nums[i]], result)
+            num = numsList[i]
+            result.append(temp + [num])
+            numsDict[num] -= 1
+            self.find(N, numsDict, numsList, i, temp + [num], result)
+            numsDict[num] += 1
+
+# class Solution(object):
+#     def subsets(self, nums):
+#         nums.sort()
+#         result = [[]]
+#         N, past = len(nums), -99
+#         for i in range(N):
+#             if nums[i] == past:
+#                 continue
+#             result.append([nums[i]])
+#             past = nums[i]
+#             self.find(N, nums, i + 1, [nums[i]], result)
+
+#         return result
+
+#     def find(self, N, nums, idx, temp, result):
+#         if idx == N:
+#             return
+
+#         past = -99
+#         for i in range(idx, N):
+#             if nums[i] == past:
+#                 continue
+#             result.append(temp + [nums[i]])
+#             past = nums[i]
+#             self.find(N, nums, i + 1, temp + [nums[i]], result)
