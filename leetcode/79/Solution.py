5f221c46a1bb3d09e5f3ec9c8a5914dd2255a864
jamie <rmk1075@gmail.com>
2021-03-23 21:04:13 +0900

    leetcode_79_upgrade_add pretest

--- leetcode/79/Solution.py
+++ leetcode/79/Solution.py
@@ -7,6 +7,22 @@ class Solution(object):
         if m * n < l:
             return False
 
+        wordDict = dict()
+        for w in word:
+            if w in wordDict:
+                wordDict[w] += 1
+            else:
+                wordDict[w] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in wordDict:
+                    wordDict[board[i][j]] -= 1
+
+        for w in wordDict:
+            if 0 < wordDict[w]:
+                return False
+
         dx, dy = [-1, 0, 1, 0], [0, -1, 0, 1]
         for i in range(m):
             for j in range(n):
@@ -30,3 +46,36 @@ class Solution(object):
             if self.dfs(board, visited, dx, dy, m, n, word, l, idx + 1, cx, cy):
                 return True
             visited[cx][cy] = False
+
+# class Solution(object):
+#     def exist(self, board, word):
+#         m, n = len(board), len(board[0])
+#         visited = [[False for _ in range(n)] for _ in range(m)]
+
+#         l = len(word)
+#         if m * n < l:
+#             return False
+
+#         dx, dy = [-1, 0, 1, 0], [0, -1, 0, 1]
+#         for i in range(m):
+#             for j in range(n):
+#                 if board[i][j] == word[0]:
+#                     visited[i][j] = True
+#                     if self.dfs(board, visited, dx, dy, m, n, word, l, 1, i, j):
+#                         return True
+#                     visited[i][j] = False
+#         return False
+
+#     def dfs(self, board, visited, dx, dy, m, n, word, l, idx, x, y):
+#         if idx == l:
+#             return True
+
+#         w = word[idx]
+#         for d in range(4):
+#             cx, cy = x + dx[d], y + dy[d]
+#             if cx == -1 or cx == m or cy == -1 or cy == n or visited[cx][cy] or board[cx][cy] != w:
+#                 continue
+#             visited[cx][cy] = True
+#             if self.dfs(board, visited, dx, dy, m, n, word, l, idx + 1, cx, cy):
+#                 return True
+#             visited[cx][cy] = False
