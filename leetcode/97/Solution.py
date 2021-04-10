a41aff687f2fdf39e7dc0f5bdcfbc7b30b248deb
jamie <rmk1075@gmail.com>
2021-04-10 19:57:18 +0900

    leetcode_97_Interleaving String_dp_python

---
+++ leetcode/97/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        a, b, c, m, n, l = 0, 0, 0, len(s1), len(s2), len(s3)
+        if l != m + n:
+            return False
+
+        dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[0][0] = True
+        for i in range(1, m + 1):
+            dp[i][0] = dp[i - 1][0] and (s1[i - 1] == s3[i - 1])
+        for i in range(1, n + 1):
+            dp[0][i] = dp[0][i - 1] and (s2[i - 1] == s3[i - 1])
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = (dp[i][j - 1] and (s2[j - 1] == s3[i + j - 1])) or (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
+        return dp[m][n]
