552b3917969457ee6f072417191c7f0b48eababa
jamie <rmk1075@gmail.com>
2021-04-24 10:57:52 +0900

    leetcode_112_update_bfs

--- leetcode/112/Solution.py
+++ leetcode/112/Solution.py
@@ -8,7 +8,23 @@ class Solution:
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
         if not root:
             return False
-        return self.dfs(root, targetSum, root.val)
+
+        queue = [(root, root.val)]
+        while queue:
+            current = queue.pop(0)
+            val = current.val
+            if not (current.left or current.right):
+                if targetSum == val:
+                    return True
+                continue
+            
+            if current.left:
+                queue.append((current.left, val + current.left.val))
+            
+            if current.right:
+                queue.append((current.right, val + current.right.val))
+
+        return False
     
     def dfs(self, node: TreeNode, targetSum: int, val: int) -> bool:
         if not (node.left or node.right):
@@ -22,4 +38,31 @@ class Solution:
             right = self.dfs(node.right, targetSum, val + node.right.val)
         
         return left or right
+
+
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+#         if not root:
+#             return False
+#         return self.dfs(root, targetSum, root.val)
+    
+#     def dfs(self, node: TreeNode, targetSum: int, val: int) -> bool:
+#         if not (node.left or node.right):
+#             return targetSum == val
+        
+#         left, right = False, False
+#         if node.left:
+#             left = self.dfs(node.left, targetSum, val + node.left.val)
+        
+#         if node.right:
+#             right = self.dfs(node.right, targetSum, val + node.right.val)
+        
+#         return left or right
 
