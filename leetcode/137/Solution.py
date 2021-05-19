b6b774ffb2d52e8d2473822bdda1d31500fc9454
jamie <rmk1075@gmail.com>
2021-05-19 13:47:04 +0900

    leetcode_137_bit-manipulation

--- leetcode/137/Solution.py
+++ leetcode/137/Solution.py
@@ -1,12 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums_dict = {}
+        one, two = 0, 0
         for num in nums:
-            if num in nums_dict.keys():
-                nums_dict[num] += 1
-            else:
-                nums_dict[num] = 1
+            one, two, three = one ^ num, two | (one & num), two & num
+            one, two = one & ~three, two & ~three
+        return one
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         nums_dict = {}
+#         for num in nums:
+#             if num in nums_dict.keys():
+#                 nums_dict[num] += 1
+#             else:
+#                 nums_dict[num] = 1
         
-        for num in nums_dict.keys():
-            if nums_dict[num] == 1:
-                return num
+#         for num in nums_dict.keys():
+#             if nums_dict[num] == 1:
+#                 return num
