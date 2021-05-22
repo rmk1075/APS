9ecd9f89b98cd0bb92ebeab11dd26dd4d5022ebf
jamie <rmk1075@gmail.com>
2021-05-22 13:21:06 +0900

    leetcode_139_update

--- leetcode/139/Solution.py
+++ leetcode/139/Solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         N = len(s)
-        dp = [False] * (N + 1)
-        dp[0] = True
+        dp = [True] + [False] * N
+        wordDict = set(wordDict)
         for i in range(1, N + 1):
-            for j in range(i):
-                print(dp[j - 1], s[j : i])
-                dp[i] |= dp[j - 1] and (s[j : i] in wordDict)
-        print(dp)
+            for j in range(1, i + 1):
+                dp[i] |= dp[j - 1] and (s[j - 1 : i] in wordDict)
+                if dp[i]:
+                    break
         return dp[N]
