9e74df0574ab3e7fec9cf7cc05495f48d3ae5bb5
jamie <rmk1075@gmail.com>
2021-05-25 21:40:21 +0900

    leetcode_141_update_two pointers

--- leetcode/141/Solution.py
+++ leetcode/141/Solution.py
@@ -6,12 +6,24 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        node_list = []
-        node = head
-        while node:
-            node_list.append(node)
-            node = node.next
-            if node in node_list:
+        if not head:
+            return False
+
+        node1, node2 = head, head
+        while node2 and node2.next:
+            node1, node2 = node1.next, node2.next.next
+            if node1 == node2:
                 return True
-        
         return False
+
+# class Solution:
+#     def hasCycle(self, head: ListNode) -> bool:
+#         node_list = []
+#         node = head
+#         while node:
+#             node_list.append(node)
+#             node = node.next
+#             if node in node_list:
+#                 return True
+        
+#         return False
