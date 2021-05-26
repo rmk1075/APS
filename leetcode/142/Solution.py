4cebcc8b73fec91bfc63a6fad896b04fa4aaadec
jamie <rmk1075@gmail.com>
2021-05-26 22:30:27 +0900

    leetcode_142_upgrade

--- leetcode/142/Solution.py
+++ leetcode/142/Solution.py
@@ -6,11 +6,22 @@ class ListNode:
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        node_list = []
+        val, limit = 2 * 10 ** 5 + 1, 10 ** 5
         current = head
         while current:
-            node_list.append(current)
-            current = current.next
-            if current in node_list:
+            if limit < current.val:
+                current.val -= val
                 return current
+            current = current.next
         return None
+
+# class Solution:
+#     def detectCycle(self, head: ListNode) -> ListNode:
+#         node_list = []
+#         current = head
+#         while current:
+#             node_list.append(current)
+#             current = current.next
+#             if current in node_list:
+#                 return current
+#         return None
