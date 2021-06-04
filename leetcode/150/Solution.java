63398ceaa35531afab9dccb57c5f72e8a0177081
jamie <rmk1075@gmail.com>
2021-06-04 20:54:27 +0900

    leetcode_150_Evaluate Reverse Polish Notation_stack_java

---
+++ leetcode/150/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        for(String token : tokens) {
+            int a, b;
+            switch(token) {
+                case "+":
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(b + a);
+                    break;
+                case "-":
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(b - a);
+                    break;
+                case "*":
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(b * a);
+                    break;
+                case "/":
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(b / a);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
