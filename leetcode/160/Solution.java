b9a7dfe7f8b53e30d48d96320f4b70d6fbd996b6
jamie <rmk1075@gmail.com>
2021-06-11 18:10:04 +0900

    leetcode_160_update

--- leetcode/160/Solution.java
+++ leetcode/160/Solution.java
@@ -1,30 +1,41 @@
-import java.util.Stack;
-
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Stack<ListNode> stackA = new Stack<>();
-        ListNode a = headA;
-        while(a.next != null) {
-            stackA.push(a);
-            a = a.next;
+        ListNode a = headA, b = headB;
+        while(a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
-
-        Stack<ListNode> stackB = new Stack<>();
-        ListNode b = headB;
-        while(b.next != null) {
-            stackB.push(b);
-            b = b.next;
-        }
-
-        ListNode result = null;
-        while(a.equals(b)) {
-            result = a;
-
-            if(stackA.isEmpty() || stackB.isEmpty()) break;
-            a = stackA.pop();
-            b = stackB.pop();
-        }
-
-        return result;
+        return b;
     }
 }
+
+// import java.util.Stack;
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         Stack<ListNode> stackA = new Stack<>();
+//         ListNode a = headA;
+//         while(a.next != null) {
+//             stackA.push(a);
+//             a = a.next;
+//         }
+
+//         Stack<ListNode> stackB = new Stack<>();
+//         ListNode b = headB;
+//         while(b.next != null) {
+//             stackB.push(b);
+//             b = b.next;
+//         }
+
+//         ListNode result = null;
+//         while(a.equals(b)) {
+//             result = a;
+
+//             if(stackA.isEmpty() || stackB.isEmpty()) break;
+//             a = stackA.pop();
+//             b = stackB.pop();
+//         }
+
+//         return result;
+//     }
+// }
