c70b693c4d6093799a35cdb95366208e7b31f7e7
jamie <rmk1075@gmail.com>
2021-06-17 23:48:49 +0900

    boj_3954_Brainf**k 인터프리터_impl_java

--- boj/3954/Main.java
+++ boj/3954/Main.java
@@ -1,49 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int T, SM, SC, SI, pointer, MOD = 256;
-    static int[] memory;
-    static char[] commands, input;
+    static int MOD = (int)Math.pow(2, 8);
+    static int LIMIT = 50_000_000;
+    static int T, M, C, I;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
         T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         while(0 < T--) {
             st = new StringTokenizer(br.readLine());
-            SM = Integer.parseInt(st.nextToken());
-            SC = Integer.parseInt(st.nextToken());
-            SI = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+            I = Integer.parseInt(st.nextToken());
+            brainFuck(new int[M], br.readLine().toCharArray(), br.readLine().toCharArray());
+        }
+        System.out.println(sb.toString());
+    }
 
-            int inputPointer = 0;
-            pointer = 0;
-            memory = new int[SM];
-            commands = br.readLine().toCharArray();
-            input = br.readLine().toCharArray();
-            
-            // braces
-            Stack<Integer> bracesStack = new Stack<>();
-            int[][] braces = new int[SC][2];
-            int top = 0;
-            for(int i = 0; i < SC; i++) {
-                if(commands[i] == '[') {
-                    bracesStack.push(i);
-                } else if(commands[i] == ']') {
-                    braces[top][0] = bracesStack.pop();
-                    braces[top++][1] = i;
-                }
+    private static int[] setBraces(char[] program) {
+        int[] braces = new int[C];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < C; i++) {
+            if(program[i] == '[') {
+                stack.push(i);
+            } else if(program[i] == ']') {
+                int index = stack.pop();
+                braces[index] = i;
+                braces[i] = index;
             }
-            
-            int idx = 0, count = 0, loopHead = 0;
-            while(idx < SC) {
-                if(50_000_000 <= count) break;
+        }
 
-                switch(commands[idx]) {
+        return braces;
+    }
+
+    private static int getPointer(int pointer) {
+        if(pointer < 0) return M - 1;
+        else if(pointer == M) return 0;
+        return pointer;
+    }
+
+    private static void brainFuck(int[] memory, char[] program, char[] input) {
+        int pointer = 0;
+        int count = 0;
+        int i = 0, j = 0;
+        int[] braces = setBraces(program);
+        char command;
+        int step = 2;
+        int maxLocation = -1;
+        while(0 < step--) {
+            count = 0;
+            maxLocation = -1;
+            while(count < LIMIT && i < C) {
+                maxLocation = Math.max(maxLocation, i);
+                command = program[i];
+                switch(command) {
                     case '-':
                         memory[pointer] = (memory[pointer] - 1) % MOD;
                         break;
@@ -51,54 +69,135 @@ public class Main {
                         memory[pointer] = (memory[pointer] + 1) % MOD;
                         break;
                     case '<':
-                        pointer = (pointer - 1 < 0) ? SM-1 : pointer - 1;
+                        pointer = getPointer(pointer - 1);
                         break;
                     case '>':
-                        pointer = (pointer + 1 == SM) ? 0 : pointer + 1;
+                        pointer = getPointer(pointer + 1);
                         break;
-                    case '[':
-                        if(memory[pointer] == 0) {
-                            int tmp = 0;
-                            while(true) {
-                                if(braces[tmp][0] == idx) {
-                                    idx = braces[tmp][1];
-                                    break;
-                                }
-                                tmp++;
-                            }
-                        }
-                        break;
-                    case ']':
-                        if(memory[pointer] != 0) {
-                            int tmp = 0;
-                            while(tmp < SC) {
-                                if(braces[tmp][1] == idx) {
-                                    idx = braces[tmp][0];
-                                    break;
-                                }
-                                tmp++;
-                            }
-
-                            loopHead = Math.max(loopHead, tmp);
-                        }
+                    case '.':
                         break;
                     case ',':
-                        if(inputPointer < SI) memory[pointer] = input[inputPointer++] % 256;
-                        else memory[pointer] = 255;
+                        if(j == I) memory[pointer] = 255;
+                        else memory[pointer] = input[j++];
                         break;
                 }
-
-                idx++;                
+    
+                if((command == '[' && memory[pointer] == 0) || (command == ']' && memory[pointer] != 0)) i = braces[i] + 1;
+                else i++;
                 count++;
             }
-
-            if(count < 50_000_000) sb.append("Terminates\n");
-            else sb.append("Loops " + braces[loopHead][0] + " " + braces[loopHead][1] + "\n");
+    
+            if(i == C || count != LIMIT) {
+                sb.append("Terminates\n");
+                return;
+            }
         }
 
-        System.out.println(sb.toString());
+        sb.append("Loops " + braces[maxLocation] + " " + maxLocation + "\n");
     }
 }
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Stack;
+// import java.util.StringTokenizer;
+
+// public class Main {
+//     static int T, SM, SC, SI, pointer, MOD = 256;
+//     static int[] memory;
+//     static char[] commands, input;
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+//         StringTokenizer st;
+
+//         T = Integer.parseInt(br.readLine());
+//         while(0 < T--) {
+//             st = new StringTokenizer(br.readLine());
+//             SM = Integer.parseInt(st.nextToken());
+//             SC = Integer.parseInt(st.nextToken());
+//             SI = Integer.parseInt(st.nextToken());
+
+//             int inputPointer = 0;
+//             pointer = 0;
+//             memory = new int[SM];
+//             commands = br.readLine().toCharArray();
+//             input = br.readLine().toCharArray();
+            
+//             // braces
+//             Stack<Integer> bracesStack = new Stack<>();
+//             int[][] braces = new int[SC][2];
+//             int top = 0;
+//             for(int i = 0; i < SC; i++) {
+//                 if(commands[i] == '[') {
+//                     bracesStack.push(i);
+//                 } else if(commands[i] == ']') {
+//                     braces[top][0] = bracesStack.pop();
+//                     braces[top++][1] = i;
+//                 }
+//             }
+            
+//             int idx = 0, count = 0, loopHead = 0;
+//             while(idx < SC) {
+//                 if(50_000_000 <= count) break;
+
+//                 switch(commands[idx]) {
+//                     case '-':
+//                         memory[pointer] = (memory[pointer] - 1) % MOD;
+//                         break;
+//                     case '+':
+//                         memory[pointer] = (memory[pointer] + 1) % MOD;
+//                         break;
+//                     case '<':
+//                         pointer = (pointer - 1 < 0) ? SM-1 : pointer - 1;
+//                         break;
+//                     case '>':
+//                         pointer = (pointer + 1 == SM) ? 0 : pointer + 1;
+//                         break;
+//                     case '[':
+//                         if(memory[pointer] == 0) {
+//                             int tmp = 0;
+//                             while(true) {
+//                                 if(braces[tmp][0] == idx) {
+//                                     idx = braces[tmp][1];
+//                                     break;
+//                                 }
+//                                 tmp++;
+//                             }
+//                         }
+//                         break;
+//                     case ']':
+//                         if(memory[pointer] != 0) {
+//                             int tmp = 0;
+//                             while(tmp < SC) {
+//                                 if(braces[tmp][1] == idx) {
+//                                     idx = braces[tmp][0];
+//                                     break;
+//                                 }
+//                                 tmp++;
+//                             }
+
+//                             loopHead = Math.max(loopHead, tmp);
+//                         }
+//                         break;
+//                     case ',':
+//                         if(inputPointer < SI) memory[pointer] = input[inputPointer++] % 256;
+//                         else memory[pointer] = 255;
+//                         break;
+//                 }
+
+//                 idx++;                
+//                 count++;
+//             }
+
+//             if(count < 50_000_000) sb.append("Terminates\n");
+//             else sb.append("Loops " + braces[loopHead][0] + " " + braces[loopHead][1] + "\n");
+//         }
+
+//         System.out.println(sb.toString());
+//     }
+// }
 
 
 
