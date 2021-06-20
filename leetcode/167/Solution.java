20764cfa7ae3d68e04983f5f8e2f5beb0c2ff4e0
jamie <rmk1075@gmail.com>
2021-06-20 23:46:22 +0900

    leetcode_167_update by binary search

--- leetcode/167/Solution.java
+++ leetcode/167/Solution.java
@@ -1,25 +1,43 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int N = numbers.length;
-        int[] dp = new int[N];
-        for(int i = 1; i < N; i++) {
-            dp[i] = numbers[0] + numbers[i];
-            if(dp[i] == target) return new int[]{1, i + 1};
-        }
-
         int[] result = new int[2];
-        loop: for(int i = 1; i < N; i++) {
-            int diff = numbers[i] - numbers[i - 1];
-            for(int j = i + 1; j < N; j++) {
-                dp[j] += diff;
-                if(dp[j] == target) {
-                    result[0] = i + 1;
-                    result[1] = j + 1;
-                    break loop;
-                }
-            }
+        int left = 0, right = numbers.length - 1;
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) {
+                result[0] = left + 1;
+                result[1] = right + 1;
+                break;
+            } else if(sum < target) left++;
+            else right--;
         }
 
         return result;
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int N = numbers.length;
+//         int[] dp = new int[N];
+//         for(int i = 1; i < N; i++) {
+//             dp[i] = numbers[0] + numbers[i];
+//             if(dp[i] == target) return new int[]{1, i + 1};
+//         }
+
+//         int[] result = new int[2];
+//         loop: for(int i = 1; i < N; i++) {
+//             int diff = numbers[i] - numbers[i - 1];
+//             for(int j = i + 1; j < N; j++) {
+//                 dp[j] += diff;
+//                 if(dp[j] == target) {
+//                     result[0] = i + 1;
+//                     result[1] = j + 1;
+//                     break loop;
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// }
