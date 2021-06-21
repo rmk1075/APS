9d40ce1023133b9e44a2c9ea967c666e16085533
jamie <rmk1075@gmail.com>
2021-06-21 21:57:09 +0900

    leetcode_168_update

--- leetcode/168/Solution.java
+++ leetcode/168/Solution.java
@@ -1,16 +1,29 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        String result = "";
-        while(0 < columnNumber) {
-            int value = columnNumber % 26;
-            if(value == 0) {
-                result = "Z" + result;
-                columnNumber = columnNumber / 26 - 1;
-            } else {
-                result = (char)(value - 1 + 'A') + result;
-                columnNumber /= 26;
-            }
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0) {
+            char lastLetter = (char)((columnNumber - 1) % 26 + 'A');
+            columnNumber = (columnNumber - 1) / 26;
+            
+            sb.append(lastLetter);
         }
-        return result;
+        return sb.reverse().toString();
     }
 }
+
+// class Solution {
+//     public String convertToTitle(int columnNumber) {
+//         String result = "";
+//         while(0 < columnNumber) {
+//             int value = columnNumber % 26;
+//             if(value == 0) {
+//                 result = "Z" + result;
+//                 columnNumber = columnNumber / 26 - 1;
+//             } else {
+//                 result = (char)(value - 1 + 'A') + result;
+//                 columnNumber /= 26;
+//             }
+//         }
+//         return result;
+//     }
+// }
