e8e234dc462d73edb1fb48aaf9b8951cbb133589
jamie <rmk1075@gmail.com>
2021-07-06 21:25:24 +0900

    leetcode_180_update

--- leetcode/180/Solution.sql
+++ leetcode/180/Solution.sql
@@ -1,16 +1,25 @@
-SELECT DISTINCT(CONSECUTIVENUMS)
-  FROM (
-    SELECT ID
-        ,  NUM AS CONSECUTIVENUMS
-      FROM LOGS L1
-     WHERE NUM = (
-        SELECT L2.NUM
-          FROM LOGS L2
-         WHERE L2.ID = L1.ID + 1     
-        )
-       AND NUM = (
-        SELECT L3.NUM
-          FROM LOGS L3
-         WHERE L3.ID = L1.ID + 2
-    )
-  )
+SELECT DISTINCT(L1.NUM) AS CONSECUTIVENUMS
+  FROM LOGS L1
+    ,  LOGS L2
+    ,  LOGS L3
+ WHERE L1.ID = L2.ID - 1
+   AND L2.ID = L3.ID - 1
+   AND L1.NUM = L2.NUM
+   AND L2.NUM = L3.NUM
+
+-- SELECT DISTINCT(CONSECUTIVENUMS)
+--   FROM (
+--     SELECT ID
+--         ,  NUM AS CONSECUTIVENUMS
+--       FROM LOGS L1
+--      WHERE NUM = (
+--         SELECT L2.NUM
+--           FROM LOGS L2
+--          WHERE L2.ID = L1.ID + 1     
+--         )
+--        AND NUM = (
+--         SELECT L3.NUM
+--           FROM LOGS L3
+--          WHERE L3.ID = L1.ID + 2
+--     )
+--   )
