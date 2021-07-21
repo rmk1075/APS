805ef490b2276c08e92e32f46f2cb5255c9a9a5a
jamie <rmk1075@gmail.com>
2021-07-21 22:01:26 +0900

    leetcode_201_update_bitManipulation_Math

--- leetcode/201/Solution.java
+++ leetcode/201/Solution.java
@@ -1,11 +1,18 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        int result = left;
-        long i = left;
-        for(i = i + 1; i <= right; i++) {
-            result &= i;
-            if(result == 0) break;
-        }
-        return result;
+        while(left < right) right &= (right - 1);
+        return right;
     }
 }
+
+// class Solution {
+//     public int rangeBitwiseAnd(int left, int right) {
+//         int result = left;
+//         long i = left;
+//         for(i = i + 1; i <= right; i++) {
+//             result &= i;
+//             if(result == 0) break;
+//         }
+//         return result;
+//     }
+// }
