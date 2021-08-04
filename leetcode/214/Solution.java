56796fa2019c3a846ec065ed19958a53018a868a
jamie <rmk1075@gmail.com>
2021-08-04 22:38:41 +0900

    leetcode_214_upgrade_KMP

--- leetcode/214/Solution.java
+++ leetcode/214/Solution.java
@@ -1,29 +1,44 @@
 class Solution {
     public String shortestPalindrome(String s) {
         int len = s.length();
+        int left = 0, right = len - 1;
         char[] sToChar = s.toCharArray();
-
-        int right = len - 1;
-        for(; 0 < right; right--) {
-            if(sToChar[0] != sToChar[right]) continue;
-            if(isPailndrome(sToChar, right)) break;
+        for(; -1 < right; right--) {
+            if(sToChar[left] == sToChar[right]) left++;
         }
+        if(left == len) return s;
 
-        if(right < len) {
-            StringBuilder addition = new StringBuilder();
-            addition.append(s.substring(right + 1));
-            s = addition.reverse().toString() + s;
-        }
-        return s;
-    }
-
-    public boolean isPailndrome(char[] words, int right) {
-        int left = 0;
-        while(left < right) {
-            if(words[left] != words[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
+        String suffix = s.substring(left);
+        return new StringBuilder(suffix).reverse().append(shortestPalindrome(s.substring(0, left))).append(suffix).toString();
     }
 }
+
+// class Solution {
+//     public String shortestPalindrome(String s) {
+//         int len = s.length();
+//         char[] sToChar = s.toCharArray();
+
+//         int right = len - 1;
+//         for(; 0 < right; right--) {
+//             if(sToChar[0] != sToChar[right]) continue;
+//             if(isPailndrome(sToChar, right)) break;
+//         }
+
+//         if(right < len) {
+//             StringBuilder addition = new StringBuilder();
+//             addition.append(s.substring(right + 1));
+//             s = addition.reverse().toString() + s;
+//         }
+//         return s;
+//     }
+
+//     public boolean isPailndrome(char[] words, int right) {
+//         int left = 0;
+//         while(left < right) {
+//             if(words[left] != words[right]) return false;
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// }
