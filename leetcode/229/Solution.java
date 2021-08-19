b6437eda145938a0eaa86b4c79fd4d3980421a10
jamie <rmk1075@gmail.com>
2021-08-19 22:34:08 +0900

    leetcode_229_update

--- leetcode/229/Solution.java
+++ leetcode/229/Solution.java
@@ -1,24 +1,48 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int N = nums.length;
-        int ths = N / 3 + 1;
-        for(int num : nums) {
-            if(map.containsKey(num)) {
-                int temp = map.get(num) + 1;
-                map.put(num, temp);
-                if(temp == ths) result.add(num);
-            } else {
-                map.put(num, 1);
-                if(1 == ths) result.add(num);
+        Arrays.sort(nums);
+        int N = nums.length / 3;
+
+        int past = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == past) count++;
+            else {
+                if(N < count) result.add(past);
+                past = nums[i];
+                count = 1;
             }
         }
-
+        if(N < count) result.add(past);
         return result;
     }
 }
+
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.Map;
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         List<Integer> result = new ArrayList<>();
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int N = nums.length;
+//         int ths = N / 3 + 1;
+//         for(int num : nums) {
+//             if(map.containsKey(num)) {
+//                 int temp = map.get(num) + 1;
+//                 map.put(num, temp);
+//                 if(temp == ths) result.add(num);
+//             } else {
+//                 map.put(num, 1);
+//                 if(1 == ths) result.add(num);
+//             }
+//         }
+
+//         return result;
+//     }
+// }
