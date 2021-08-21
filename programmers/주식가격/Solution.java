136b5352913c1574d438fd7362f24ca8abe735fc
jamie <rmk1075@gmail.com>
2021-08-21 15:24:18 +0900

    programmers_주식가격_update

--- programmers/주식가격/Solution.java
+++ programmers/주식가격/Solution.java
@@ -7,29 +7,50 @@ class Solution {
         int N = prices.length;
         int[] answer = new int[N];
 
-        // int[] {index, value}
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[1] - o1[1];
-            }
-        });
-
-        pq.offer(new int[]{0, prices[0]});
-        for(int i = 1; i < N; i++) {
+        for(int i = 0; i < N - 1; i++) {
             int price = prices[i];
-            while(pq.size() != 0 && price < pq.peek()[1]) {
-                int[] prev = pq.poll();
-                answer[prev[0]] = i - prev[0];
+            for(int j = i + 1; j < N; j++) {
+                answer[i]++;
+                if(prices[j] < price) break;
             }
-            pq.offer(new int[]{i, price});
-        }
-
-        while(pq.size() != 0) {
-            int[] prev = pq.poll();
-            answer[prev[0]] = N - 1 - prev[0];
         }
 
         return answer;
     }
 }
+
+// import java.util.Comparator;
+// import java.util.List;
+// import java.util.PriorityQueue;
+
+// class Solution {
+//     public int[] solution(int[] prices) {
+//         int N = prices.length;
+//         int[] answer = new int[N];
+
+//         // int[] {index, value}
+//         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+//             @Override
+//             public int compare(int[] o1, int[] o2) {
+//                 return o2[1] - o1[1];
+//             }
+//         });
+
+//         pq.offer(new int[]{0, prices[0]});
+//         for(int i = 1; i < N; i++) {
+//             int price = prices[i];
+//             while(pq.size() != 0 && price < pq.peek()[1]) {
+//                 int[] prev = pq.poll();
+//                 answer[prev[0]] = i - prev[0];
+//             }
+//             pq.offer(new int[]{i, price});
+//         }
+
+//         while(pq.size() != 0) {
+//             int[] prev = pq.poll();
+//             answer[prev[0]] = N - 1 - prev[0];
+//         }
+
+//         return answer;
+//     }
+// }
