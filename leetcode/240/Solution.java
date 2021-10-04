3499c279e03cc353f22bb75909c113e85514aaed
jamie <rmk1075@gmail.com>
2021-10-04 14:19:15 +0900

    leetcode_240_update

--- leetcode/240/Solution.java
+++ leetcode/240/Solution.java
@@ -2,13 +2,28 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int M = matrix.length;
         int N = matrix[0].length;
-        return dfs(matrix, M, N, M - 1, 0, target);
-    }
-
-    public boolean dfs(int[][] matrix, int M, int N, int r, int c, int target) {
-        if(r < 0 || r == M || c < 0 || c == N) return false;
-        if(matrix[r][c] == target) return true;
-        if(matrix[r][c] < target) return dfs(matrix, M, N, r, c + 1, target);
-        return dfs(matrix, M, N, r - 1, c, target);
+        int r = M - 1;
+        int c = 0;
+        while(-1 < r && c < N) {
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] < target) c++;
+            else r--;
+        }
+        return false;
     }
 }
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int M = matrix.length;
+//         int N = matrix[0].length;
+//         return dfs(matrix, M, N, M - 1, 0, target);
+//     }
+
+//     public boolean dfs(int[][] matrix, int M, int N, int r, int c, int target) {
+//         if(r < 0 || r == M || c < 0 || c == N) return false;
+//         if(matrix[r][c] == target) return true;
+//         if(matrix[r][c] < target) return dfs(matrix, M, N, r, c + 1, target);
+//         return dfs(matrix, M, N, r - 1, c, target);
+//     }
+// }
