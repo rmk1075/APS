a7dde651195e7d550325e978eb4a3dcfd8c87139
jamie <rmk1075@gmail.com>
2021-10-11 12:22:54 +0900

    leetcode_260_Single Number III_hashmap_java

---
+++ leetcode/260/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int count = 0;
+        int[] result = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) map.put(num, map.containsKey(num) ? 2 : 1);
+        for(int num : map.keySet()) {
+            if(map.get(num) == 1) {
+                result[count++] = num;
+                if(count == 2) break;
+            }
+        }
+
+        return result;
+    }
+}
