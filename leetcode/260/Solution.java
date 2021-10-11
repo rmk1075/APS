e96e6d42abc9ec0cf9069c9b6265a3c914ac0186
jamie <rmk1075@gmail.com>
2021-10-11 12:37:54 +0900

    leetcode_260_update

--- leetcode/260/Solution.java
+++ leetcode/260/Solution.java
@@ -1,20 +1,35 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int count = 0;
-        int[] result = new int[2];
+        int bit = 0;
+        for(int num : nums) bit ^= num;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) map.put(num, map.containsKey(num) ? 2 : 1);
-        for(int num : map.keySet()) {
-            if(map.get(num) == 1) {
-                result[count++] = num;
-                if(count == 2) break;
-            }
+        int diff = bit & (-bit);
+        int result = 0;
+        for(int num : nums) {
+            if((num & diff) == 0) result ^= num;
         }
 
-        return result;
+        return new int[]{result, bit^result};
     }
 }
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// class Solution {
+//     public int[] singleNumber(int[] nums) {
+//         int count = 0;
+//         int[] result = new int[2];
+
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int num : nums) map.put(num, map.containsKey(num) ? 2 : 1);
+//         for(int num : map.keySet()) {
+//             if(map.get(num) == 1) {
+//                 result[count++] = num;
+//                 if(count == 2) break;
+//             }
+//         }
+
+//         return result;
+//     }
+// }
