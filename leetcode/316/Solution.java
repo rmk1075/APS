0055bc628f742cec2ec8453616f1c4aa40e8ef08
jamie <rmk1075@gmail.com>
2022-01-13 17:46:15 +0900

    leetcode_316_update_change stack to deque

--- leetcode/316/Solution.java
+++ leetcode/316/Solution.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
 
 class Solution {
     public String removeDuplicateLetters(String s) {
@@ -11,18 +9,18 @@ class Solution {
         for(char ch : chs) counts[ch - 'a']++;
 
         boolean[] contained = new boolean[26];
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> queue = new LinkedList<>();
         for(int i = 0; i < N; i++) {
             char ch = chs[i];
             counts[ch - 'a']--;
             if(contained[ch - 'a']) continue;
-            while(!stack.isEmpty() && ch < stack.peek() && counts[stack.peek() - 'a'] != 0) contained[stack.pop() - 'a'] = false;
+            while(!queue.isEmpty() && ch < queue.peekLast() && counts[queue.peekLast() - 'a'] != 0) contained[queue.pollLast() - 'a'] = false;
             contained[ch - 'a'] = true;
-            stack.push(ch);
+            queue.offer(ch);
         }
 
         StringBuffer result = new StringBuffer();
-        while(!stack.isEmpty()) result.insert(0, stack.pop());
+        while(!queue.isEmpty()) result.append(queue.poll());
         return result.toString();
     }
 }
