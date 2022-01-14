734c0dabf012daa5a58fb44d35a96ed922b4b337
jamie <rmk1075@gmail.com>
2022-01-14 09:41:23 +0900

    leetcode_318_Maximum Product of Word Lengths_bit_java

---
+++ leetcode/318/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+
+class Solution {
+    public int maxProduct(String[] words) {
+        int N = words.length;
+        int i = 0;
+        int[] bits = new int[N];
+        for(String word : words) {
+            for(char w : word.toCharArray()) bits[i] |= 1 << (w - 'a');
+            i++;
+        }
+
+        int result = 0;
+        for(i = 0; i < N - 1; i++) {
+            for(int j = i + 1; j < N; j++) {
+                if((bits[i] & bits[j]) == 0) result = Math.max(result, words[i].length() * words[j].length());
+            }
+        }
+        return result;
+    }
+}
