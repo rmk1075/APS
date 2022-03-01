4208d51e8cbeb34d825a3c111b2e51eafcee737a
jamie <rmk1075@gmail.com>
2022-03-02 00:14:17 +0900

    programmers_다단계 칫솔 판매_효율 향상

--- programmers/다단계_칫솔_판매/Solution.java
+++ programmers/다단계_칫솔_판매/Solution.java
@@ -3,53 +3,105 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+class Node {
+    int value = 0;
+    List<Integer> taxList = new LinkedList<>();
+}
+
 class Solution {
     static final int PRICE = 100;
-    Map<String, Integer> map = new HashMap<>();
+    int E, S;
+    Map<String, Node> map = new HashMap<>();
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        for(String e : enroll) {
-            map.put(e, 0);
+        E = enroll.length;
+        S = seller.length;
+        for(String e : enroll) map.put(e, new Node());
+        for(int i = 0; i < S; i++) {
+            int value = amount[i] * PRICE;
+            int tax = (int)Math.ceil(value / 10);
+
+            Node node = map.get(seller[i]);
+            node.value += value - tax;
+            if(tax != 0) node.taxList.add(tax);
         }
 
-        for(int i = 0; i < referral.length; i++) {
-            String parent = referral[i];
-            if(parent.equals("-")) {
-                dfs(enroll[i], enroll, referral, seller, amount);
-            }
+        for(int i = 0; i < E; i++) {
+            if(referral[i].equals("-")) dfs(enroll[i], enroll, referral, seller, amount);
         }
         
         int[] answer = new int[enroll.length];
-        for(int i = 0; i < enroll.length; i++) {
-            answer[i] = map.get(enroll[i]);
-        }
+        for(int i = 0; i < enroll.length; i++) answer[i] = map.get(enroll[i]).value;
         return answer;
     }
 
-    public List<Integer> dfs(String node, String[] enroll, String[] referral, String[] seller, int[] amounts) {
-        int amount = 0;
-        List<Integer> taxList = new LinkedList<>();
+    public List<Integer> dfs(String name, String[] enroll, String[] referral, String[] seller, int[] amounts) {
+        Node node = map.get(name);
         for(int i = 0; i < referral.length; i++) {
-            if(referral[i].equals(node)) {
+            if(referral[i].equals(name)) {
                 List<Integer> tempTaxList = dfs(enroll[i], enroll, referral, seller, amounts);
                 for(int temp : tempTaxList) {
                     int tax = (int)Math.ceil(temp / 10);
-                    amount += temp - tax;
-                    if(tax != 0) taxList.add(tax);
+                    node.value += temp - tax;
+                    if(tax != 0) node.taxList.add(tax);
                 }
             }
         }
-
-        for(int i = 0; i < seller.length; i++) {
-            if(seller[i].equals(node)) {
-                int temp = amounts[i] * PRICE;
-                int tax = (int)Math.ceil(temp / 10);
-                amount += temp - tax;
-                if(tax != 0) taxList.add(tax);
-            }
-        }
-
-        map.put(node, amount);
-        return taxList;
-
+        return node.taxList;
     }
 }
+
+// import java.util.HashMap;
+// import java.util.LinkedList;
+// import java.util.List;
+// import java.util.Map;
+
+// class Solution {
+//     static final int PRICE = 100;
+//     Map<String, Integer> map = new HashMap<>();
+//     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+//         for(String e : enroll) {
+//             map.put(e, 0);
+//         }
+
+//         for(int i = 0; i < referral.length; i++) {
+//             String parent = referral[i];
+//             if(parent.equals("-")) {
+//                 dfs(enroll[i], enroll, referral, seller, amount);
+//             }
+//         }
+        
+//         int[] answer = new int[enroll.length];
+//         for(int i = 0; i < enroll.length; i++) {
+//             answer[i] = map.get(enroll[i]);
+//         }
+//         return answer;
+//     }
+
+//     public List<Integer> dfs(String node, String[] enroll, String[] referral, String[] seller, int[] amounts) {
+//         int amount = 0;
+//         List<Integer> taxList = new LinkedList<>();
+//         for(int i = 0; i < referral.length; i++) {
+//             if(referral[i].equals(node)) {
+//                 List<Integer> tempTaxList = dfs(enroll[i], enroll, referral, seller, amounts);
+//                 for(int temp : tempTaxList) {
+//                     int tax = (int)Math.ceil(temp / 10);
+//                     amount += temp - tax;
+//                     if(tax != 0) taxList.add(tax);
+//                 }
+//             }
+//         }
+
+//         for(int i = 0; i < seller.length; i++) {
+//             if(seller[i].equals(node)) {
+//                 int temp = amounts[i] * PRICE;
+//                 int tax = (int)Math.ceil(temp / 10);
+//                 amount += temp - tax;
+//                 if(tax != 0) taxList.add(tax);
+//             }
+//         }
+
+//         map.put(node, amount);
+//         return taxList;
+
+//     }
+// }
