d0158dfe83a9f2360858c09c3684e363d83d7acf
jamie <rmk1075@gmail.com>
2022-03-11 14:05:13 +0900

    programmers_공 이동 시뮬레이션_import 삭제

--- programmers/공_이동_시뮬레이션/Solution.java
+++ programmers/공_이동_시뮬레이션/Solution.java
@@ -1,11 +1,3 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-
 /**
  * overview
  * 
