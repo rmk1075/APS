b8a284f11e334bf1335229dd3bceff155578d79d
jamie <rmk1075@gmail.com>
2022-07-27 01:21:26 +0900

    boj_1706_크로스워드_impl_java

--- boj/1706/Main.java
+++ boj/1706/Main.java
@@ -1,80 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int R, C;
-    static String word = "", ans;
-    static String[][] puzzles;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        R = sc.nextInt();
-        C = sc.nextInt();
-        sc.nextLine();
-        puzzles = new String[R][C];
+    static char[][] arr;
+    static String minStr = "zzzzzzzzzzzzzzzzzzzzz";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new char[R][C];
+        for(int i = 0; i < R; i++) arr[i] = br.readLine().toCharArray();
         for(int i = 0; i < R; i++) {
-            puzzles[i] = sc.nextLine().split("");
-        }
-        sc.close();
-
-        // row
-        for(int i = 0; i < R; i++) {
-            String tempWord = "";
-            for(int j = 0; j < C; j++) {
-                if(puzzles[i][j].equals("#")) {
-                    if(tempWord == "") continue;
-
-                    check(tempWord);
-                    tempWord = "";
-                } else tempWord += puzzles[i][j];
-            }
-
-            if(!tempWord.equals("")) {
-                check(tempWord);
-                tempWord = "";
+            StringBuffer sb = new StringBuffer();
+            for(int j = 0; j < C; j++) check(sb, arr[i][j]);
+            if(1 < sb.length()) {
+                String temp = sb.toString();
+                if(0 < minStr.compareTo(temp)) minStr = temp;
             }
         }
 
-        // col
         for(int i = 0; i < C; i++) {
-            String tempWord = "";
-            for(int j = 0; j < R; j++) {
-                if(puzzles[j][i].equals("#")) {
-                    if(tempWord == "") continue;
-
-                    check(tempWord);
-                    tempWord = "";
-                } else tempWord += puzzles[j][i];
-            }
-
-            if(!tempWord.equals("")) {
-                check(tempWord);    
-                tempWord = "";
+            StringBuffer sb = new StringBuffer();
+            for(int j = 0; j < R; j++) check(sb, arr[j][i]);
+            if(1 < sb.length()) {
+                String temp = sb.toString();
+                if(0 < minStr.compareTo(temp)) minStr = temp;
             }
         }
-
-        System.out.println(ans);
+        System.out.println(minStr);
     }
 
-    public static void check(String tempWord) {
-        if(ans == null) ans = tempWord;
-        else {
-            // compare
-            compare(tempWord);
-        }
-    }
-
-    public static void compare(String newWord) {
-        
-        if(newWord.length() < 2) return ;
-
-        for(int i = 0; i < (ans.length() < newWord.length() ? ans.length() : newWord.length()); i++) {
-            if(ans.charAt(i) == newWord.charAt(i)) continue;
-            
-            if(newWord.charAt(i) < ans.charAt(i)) {
-                ans = newWord;
-            } else return ;
-        }
-        
-        if(newWord.length() < ans.length()) ans = newWord;
+    private static void check(StringBuffer sb, char ch) {
+        if(ch == '#') {
+            if(sb.length() == 0) return;
+            if(sb.length() == 1) {
+                sb.setLength(0);
+                return;
+            }
+            String temp = sb.toString();
+            if(0 < minStr.compareTo(temp)) minStr = temp;
+            sb.setLength(0);
+        } else sb.append(ch);
     }
 }
