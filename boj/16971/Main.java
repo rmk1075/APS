47b4baf83b2d33c9f163487370af1c8a0c0996fd
jamie <rmk1075@gmail.com>
2024-01-29 21:37:13 +0900

    modify boj_16971

--- boj/16971/Main.java
+++ boj/16971/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M, sum, result;
@@ -15,14 +16,14 @@ public class Main {
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] NM = br.readLine().split(" ");
-        N = Integer.parseInt(NM[0]);
-        M = Integer.parseInt(NM[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         arr = new int[N][M];
         for(int i = 0; i < N; i++) {
-            String[] line = br.readLine().split(" ");
-            for(int j = 0; j < M; j++) arr[i][j] = Integer.parseInt(line[j]);
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) arr[i][j] = Integer.parseInt(st.nextToken());
         }
 
         calRows();
