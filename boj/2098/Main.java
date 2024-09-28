dca4b1a60490617d18e490f790ca3814756509a5
jamie <rmk1075@gmail.com>
2024-09-28 13:11:03 +0900

    boj_2098_외판원_순회

--- boj/2098/Main.java
+++ boj/2098/Main.java
@@ -5,30 +5,54 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, map[][], dp[][];
+    static int N;
+    static int[][] map;
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
+        init();
+        int result = find(0, 1);
+        System.out.println(result);
+    }
+
+    private static void init() throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        dp = new int[N][1 << N]; // {current, visited}
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Arrays.fill(dp[i], 16000001);
-            for(int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        System.out.println(find(0, 1));
+        dp = new int[N][1 << N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
     }
 
-    public static int find(int cur, int visited) {
-        if(visited == (1 << N)-1) return (map[cur][0] == 0) ? 16000001 : map[cur][0];
-        if(dp[cur][visited] != 16000001) return dp[cur][visited];
-
-        for(int i = 0; i < N; i++) {
-            if((visited & (1 << i)) != 0 || map[cur][i] == 0) continue;
-           dp[cur][visited] = Math.min(dp[cur][visited], find(i, (visited | (1 << i))) + map[cur][i]);
+    private static int find(int current, int visited) {
+        if (visited == (1 << N) - 1) {
+            dp[current][visited] = map[current][0] == 0 ? Integer.MAX_VALUE : map[current][0];
+            return dp[current][visited];
         }
 
-        return dp[cur][visited];
+        if (dp[current][visited] != -1) {
+            return dp[current][visited];
+        }
+
+        dp[current][visited] = Integer.MAX_VALUE;
+        for (int next = 1; next < N; next++) {
+            if ((visited & (1 << next)) != 0 || map[current][next] == 0) {
+                continue;
+            }
+
+            int distance = find(next, visited | (1 << next));
+            if (distance != Integer.MAX_VALUE) {
+                dp[current][visited] = Math.min(dp[current][visited], distance + map[current][next]);
+            }
+        }
+        return dp[current][visited];
     }
 }
