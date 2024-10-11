1801db7ac8dd76b35454b410b3ea92efe705c379
jamie <rmk1075@gmail.com>
2024-10-11 19:46:29 +0900

    boj_12852_1로 만들기 2

--- boj/12852/Main.java
+++ boj/12852/Main.java
@@ -1,73 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
 
 public class Main {
-    static int N, nums[];
-    static Deque<Integer> queue = new LinkedList<>();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(br.readLine());
-        nums = new int[N+1];
+        int N = Integer.parseInt(br.readLine());
 
-        queue.offer(N);
-        int size, time = 1;
-        loop: while(!queue.isEmpty()) {
-            size = queue.size();
-            while(0 < size--) {
-                int num = queue.poll();
-                if(num % 3 == 0 && 0 < num / 3 && nums[num / 3] == 0) {
-                    if(num / 3 == 1) break loop;
-                    nums[num / 3] = time;
-                    queue.offer(num / 3);
-                }
-
-                if(num % 2 == 0 && 0 < num / 2 && nums[num / 2] == 0) {
-                    if(num / 2 == 1) break loop;
-                    nums[num / 2] = time;
-                    queue.offer(num / 2);
-                }
-
-                if(0 < num - 1 && nums[num - 1] == 0) {
-                    if(num - 1 == 1) break loop;
-                    nums[num - 1] = time;
-                    queue.offer(num - 1);
-                }
-            }
-            time++;
+        int[][] mem = new int[N + 1][2];
+        for (int i = 0; i <= N; i++) {
+            mem[i] = new int[] { 0, Integer.MAX_VALUE }; // {prev index, count}
         }
-        queue.clear();
+        mem[1][1] = 0;
 
-        nums[1] = time + 1;
-        sb.append(time + "\n");
-        int cur = 1;
-        while(true) {
-            queue.offer(cur);
-
-            if(cur == N) break;
-
-            if(cur * 3 <= N && nums[cur * 3] == time - 1) {
-                cur *= 3;
-                time--;
+        for (int i = 1; i <= N; i++) {
+            if (mem[i][1] == Integer.MAX_VALUE) {
                 continue;
             }
 
-            if(cur * 2 <= N && nums[cur * 2] == time - 1) {
-                cur *= 2;
-                time--;
-                continue;
+            int count = mem[i][1];
+
+            // 3
+            if (i * 3 <= N && count + 1 < mem[i * 3][1]) {
+                mem[i * 3][0] = i;
+                mem[i * 3][1] = count + 1;
             }
 
-            if(cur + 1 <= N && nums[cur + 1] == time - 1) {
-                cur += 1;
-                time--;
+            // 2
+            if (i * 2 <= N && count + 1 < mem[i * 2][1]) {
+                mem[i * 2][0] = i;
+                mem[i * 2][1] = count + 1;
+            }
+
+            // 1
+            if (i + 1 <= N && count + 1 < mem[i + 1][1]) {
+                mem[i + 1][0] = i;
+                mem[i + 1][1] = count + 1;
             }
         }
 
-        while(!queue.isEmpty()) sb.append(queue.pollLast() + " ");
-        System.out.println(sb);
+        System.out.println(mem[N][1]);
+        int index = N;
+        while (true) {
+            System.out.print(index + " ");
+            index = mem[index][0];
+            if (index == 0) {
+                break;
+            }
+        }
     }
 }
